7f21f0c898cb946ccac6d462d4a15afae7bcbeef
kodpelle <pellewiklund@outlook.com>
2025-08-22 19:52:11 +0200

    fix(api) Endpoint Extensions to refactor Program.cs

---
+++ BuggyNotes.Api/Endpoints/EndpointExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Routing;
+
+namespace BuggyNotes.Api.Endpoints;
+
+public static class EndpointExtensions
+{
+    public static void MapEndpointsFromAssemblyContaining<T>(this IEndpointRouteBuilder app)
+        => app.MapEndpointsFromAssembly(typeof(T).Assembly);
+
+    public static void MapEndpointsFromAssembly(this IEndpointRouteBuilder app, Assembly assembly)
+    {
+        var endpointTypes = assembly.DefinedTypes
+            .Where(t => !t.IsAbstract && !t.IsInterface && typeof(IEndpoint).IsAssignableFrom(t));
+
+        foreach (var type in endpointTypes)
+        {
+            var m = type.GetMethod(nameof(IEndpoint.MapEndpoint));
+            m!.Invoke(null, new object[] { app });
+        }
+    }
+}
+
---
+++ BuggyNotes.Api/Endpoints/IEndpoint.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace BuggyNotes.Api.Endpoints;
+
+public interface IEndpoint
+{
+    static abstract void MapEndpoint(IEndpointRouteBuilder app);
+}
